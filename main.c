69d2c25777cf17ebe04128bcd7d094b850066d2b
Jose Luis <jose.luis.gonzalez.dse@uteq.edu.mx>
2018-09-07 12:13:23 -0500

    adding division

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#define RESULTADO S
 
+float division(float a, float b);
 int suma(int a, int b);
 
 int main()
 {
 printf("¡Hola mundo!\n");
+division();
 return 0;
 }
